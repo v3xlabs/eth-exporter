e41233781e3fd0f92f784c8a7a5e3a7113a848c9
Luc <luc@lucemans.nl>
2025-02-05 01:46:20 +0100

    Introduce fixed rate, uniswap v2, and v3 support

--- src/models/token.rs
+++ src/models/token.rs
@@ -70,50 +70,22 @@ impl IndexableTokenERC20 {
     }
 
     pub async fn update_usd_price(&self) {
-        if self.chain_slug == "eth"
-            && self.address
-                == Address::from_str("0xc18360217d8f7ab5e7c516566761ea12ce7f9d72").unwrap()
-        {
-            println!("Updating USD price for ENS {}", self.address);
+        if let Some(fixed_rate) = self.fixed_rate {
+            *self.usd_price.lock().await = fixed_rate;
+            return;
+        }
 
-            let pool_address: Address = "0xb169c3e8dda6456a18aefa49c58f7f53e120a9b4"
-                .parse()
-                .unwrap();
-            let pool = UniswapV2Pool::new(pool_address, self.erc20.provider().clone());
-
+        if let Some(uniswap_v2) = &self.uniswap_v2 {
+            let pool = UniswapV2Pool::new(uniswap_v2.parse().unwrap(), self.erc20.provider().clone());
             let reserves = pool.getReserves().call().await.unwrap();
-            let reserve0 =
-                reserves.reserve0.to_string().parse::<f64>().unwrap() / 10_u128.pow(6) as f64; // usdc
-            let reserve1 =
-                reserves.reserve1.to_string().parse::<f64>().unwrap() / 10_u128.pow(18) as f64; // ens
-
+            let reserve0 = reserves.reserve0.to_string().parse::<f64>().unwrap() / 10_u128.pow(6) as f64; // usdc
+            let reserve1 = reserves.reserve1.to_string().parse::<f64>().unwrap() / 10_u128.pow(18) as f64; // ens
             let price = reserve0 / reserve1;
-
-            let usd_price = price;
-
-            println!("USD price for {} is {}", self.address, usd_price);
-
-            *self.usd_price.lock().await = usd_price;
+            *self.usd_price.lock().await = price;
+            return;
         }
 
-        let usdcs = [
-            Address::from_str("0x98C23E9d8f34FEFb1B7BD6a91B7FF122F4e16F5c").unwrap(),
-            Address::from_str("0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48").unwrap(),
-        ];
-        if self.chain_slug == "eth" && usdcs.contains(&self.address) {
-            println!("Updating USD price for USDC {}", self.address);
-
-            let usd_price = 1.0;
-
-            *self.usd_price.lock().await = usd_price;
-        }
-
-        if self.chain_slug == "eth"
-            && self.address
-                == Address::from_str("0xae7ab96520de3a18e5e111b5eaab095312d7fe84").unwrap()
-        {
-            println!("Updating USD price for stETH {}", self.address);
-
+        if let Some(uniswap_v3) = &self.uniswap_v3 {
             let usdc_address: Address = "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48"
                 .parse()
                 .unwrap();
@@ -126,8 +98,7 @@ impl IndexableTokenERC20 {
                     .unwrap(),
                 self.erc20.provider().clone(),
             );
-            let weth_address = Address::from_str("0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2")
-                .unwrap();
+            let weth_address = Address::from_str(uniswap_v3).unwrap();
             let usd_price = pool
                 .quoteExactInputSingle(
                     weth_address,
@@ -147,17 +118,6 @@ impl IndexableTokenERC20 {
             let amount_in_usd = amount_in_usdc.div(10_f64.powf(6.0));
 
             *self.usd_price.lock().await = amount_in_usd;
-        }
-
-        if self.chain_slug == "polygon"
-            && self.address
-                == Address::from_str("0x625e7708f30ca75bfd92586e17077590c60eb4cd").unwrap()
-        {
-            println!("Updating USD price for USDC {}", self.address);
-
-            let usd_price = 1.0;
-
-            *self.usd_price.lock().await = usd_price;
         }
     }
 
