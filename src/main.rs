faf89f5de771f9993c09ceea303875bb24abb69b
Luc <luc@lucemans.nl>
2026-03-15 16:27:36 +0100

    z

--- src/main.rs
+++ src/main.rs
@@ -10,11 +10,11 @@ pub mod uniswap;
 pub async fn main() {
     println!("Hello, world!");
 
-    let factory_address = address!("0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f");
-    let provider = tests::get_test_provider().await;
-    let mut pairs = Box::pin(uniswap::v2::factory::fetch_all_pairs(provider, factory_address));
+    // let factory_address = address!("0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f");
+    // let provider = tests::get_test_provider().await;
+    // let mut pairs = Box::pin(uniswap::v2::factory::fetch_all_pairs(provider, factory_address));
 
-    while let Some(pair) = pairs.as_mut().next().await {
-        println!("pair: {:?}", pair);
-    }
+    // while let Some(pair) = pairs.as_mut().next().await {
+    //     println!("pair: {:?}", pair);
+    // }
 }
